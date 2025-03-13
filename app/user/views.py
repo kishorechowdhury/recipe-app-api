22c71512813a3a3fbbe1565560ce8448b24fdf4b
kishore <kishore.chowdhuri@gmail.com>
2025-03-13 11:32:08 +0530

    fixed flak8 errors

--- app/user/views.py
+++ app/user/views.py
@@ -5,7 +5,6 @@ from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
-from user.serializers import UserSerializer
 from user.serializers import (
     UserSerializer,
     AuthTokenSerializer,
@@ -21,6 +20,7 @@ class CreateTokenView(ObtainAuthToken):
     """Create a new auth token for user."""
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
     """Manage the authenticated user."""
